6268c6bd64e4573a36373372f76ac0a800f42777
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2021-10-19 16:29:04 +0200

    [use_case][env_setup] add help msg

--- simple_use_case/setup_environment.sh
+++ simple_use_case/setup_environment.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+help () {
+    echo "This script can be used to prepare the local environment for the"
+    echo "execution of the workflow, installing the required dependencies"
+    echo "(although some basic dependencies are required to be installed already)"
+    echo ""
+    echo "Usage: ./setup_environment CMAKE_PREFIX (optional) BOOST_HOME (optional)"
+    echo ""
+    echo "       CMAKE_PREFIX can be used to tell cmake further places to look for packages"
+    echo "       when installing dolfin, while BOOST_HOME can be set additionally"
+    echo "       to tell dolfin the location of the boost installation to use"
+    echo ""
+    echo "Note: It is recommended that you run this from within a virtual environment"
+    echo "      such that you can easily remove the installed python packages afterwards."
+    echo "      Please note that dolfin is not installed system-wide but into a local"
+    echo "      installation folder."
+}
+
 runAndCheck () {
     CMD=$1
     if ! $CMD; then
@@ -81,6 +98,12 @@ checkInstalledLibrary () {
     fi
 }
 
+# maybe print help message
+if [[ "$1" == *"help"* ]]; then
+    help
+    exit 1
+fi
+
 # check basic prerequisites
 checkInstalledProgram gmsh
 checkInstalledProgram cmake
@@ -99,7 +122,7 @@ BOOST_HOME=$2
 CMAKE_OPTS=""
 if [[ -n "$CM_PREFIX_PATH" ]]; then CMAKE_OPTS="-DCMAKE_PREFIX_PATH=$CM_PREFIX_PATH"; fi
 if [[ -n "$BOOST_HOME" ]]; then CMAKE_OPTS="$CMAKE_OPTS -DBOOST_HOME=$BOOST_HOME"; fi
-#
+
 # define dolfin install directory
 CUR_DIR=$(pwd)
 CMAKE_OPTS="$CMAKE_OPTS -DDOLFIN_ENABLE_HDF5=ON -DCMAKE_INSTALL_PREFIX=$CUR_DIR/dolfin/install ../"
