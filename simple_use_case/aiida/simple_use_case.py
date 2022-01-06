469f4e7862f56a47caa3fb6f1a9215913a5b0686
pdiercks <philipp.diercks@bam.de>
2022-01-06 19:00:24 +0100

    [aiida] fix gmsh task; debug mesh conversion

--- simple_use_case/aiida/simple_use_case.py
+++ simple_use_case/aiida/simple_use_case.py
@@ -4,12 +4,12 @@ from aiida.orm import List, SinglefileData
 from aiida.engine.processes.functions import shellfunction, workfunction
 
 
-@shellfunction(command="gmsh", output_filenames=["mesh.msh"])
+@shellfunction(command="gmsh", output_filenames=["mesh"])
 def gmsh(**kwargs):
     """Run the ``gmsh`` command."""
 
 
-@shellfunction(command="meshio", output_filenames=["mesh.xdmf"])
+@shellfunction(command="meshio", output_filenames=["mesh*"])
 def meshio(**kwargs):
     """Run the ``meshio`` command."""
 
@@ -25,11 +25,13 @@ def pvbatch(**kwargs):
 
 
 @workfunction
-def workflow(geometry, poisson_script, post_processing_script):
+def workflow(geometry):
     """Simple workflow to solve Poisson equation for a geometry."""
 
     # Generate the mesh from the geometry.
-    arguments = List(["-2", "-order", "1", "-format", "msh2", "{geometry}"])
+    arguments = List(
+        ["-2", "-order", "1", "-format", "msh2", "{geometry}", "-o", "mesh"]
+    )
     results_gmsh = gmsh(arguments=arguments, geometry=geometry)
 
     # Convert the mesh to XDMF format
@@ -37,28 +39,30 @@ def workflow(geometry, poisson_script, post_processing_script):
     results_meshio = meshio(arguments=arguments, mesh=results_gmsh["output"])
 
     # Solve Poisson
-    arguments = List(["{script}", "--mesh", "{mesh}", "--degree", "2"])
-    results_poisson = poisson(
-        arguments=arguments, script=poisson_script, mesh=results_meshio["output"]
-    )
+    # arguments = List(["{script}", "--mesh", "{mesh}", "--degree", "2"])
+    # results_poisson = poisson(
+    #     arguments=arguments, script=poisson_script, mesh=results_meshio["output"]
+    # )
 
     # Postprocessing: plot over line
-    arguments = List(["{script}", "{pvd}"])
-    results_pvbatch = pvbatch(
-        script=post_processing_script, pvd=results_poisson["output"]
-    )
+    # arguments = List(["{script}", "{pvd}"])
+    # results_pvbatch = pvbatch(
+    #     script=post_processing_script, pvd=results_poisson["output"]
+    # )
 
-    return results_pvbatch["output"]
+    # return results_pvbatch["output"]
+    return results_gmsh
 
 
 if __name__ == "__main__":
     geometry = SinglefileData(Path("../source/unit_square.geo").resolve())
-    poisson_script = SinglefileData(Path("../source/poisson.py").resolve())
-    post_processing_script = SinglefileData(
-        Path("../source/postprocessing.py").resolve()
-    )
-    results, node = workflow.run_get_node(
-        geometry, poisson_script, post_processing_script
-    )
+    # poisson_script = SinglefileData(Path("../source/poisson.py").resolve())
+    # post_processing_script = SinglefileData(
+    #     Path("../source/postprocessing.py").resolve()
+    # )
+    # results, node = workflow.run_get_node(
+    #     geometry, poisson_script, post_processing_script
+    # )
+    results, node = workflow.run_get_node(geometry)
     print(f"Workflow {node} finished.")
     print(f"Results: {results}")
