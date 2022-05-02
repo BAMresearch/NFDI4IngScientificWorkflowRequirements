aa91557beeea4904677538ff5a6c73d9f3fb8aa2
larsg <lars.griem@kit.edu>
2022-05-02 17:07:54 +0200

    GITHUB_WORKSPACE check

--- simple_use_case/kadistudio/install_components.sh
+++ simple_use_case/kadistudio/install_components.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-get install -y git
-python3 -m pip install git+https://gitlab.com/lars153/workflow-nodes.git@e1e0f20beadaf0ff13e7fbfec2bbd3c6cd141781
+python3 -m pip install git+https://gitlab.com/lars153/workflow-nodes.git@659a64bd127c1b61653c2396798419529fc91ffb
 wget https://gitlab.com/iam-cms/workflows/process-engine/-/jobs/1764662488/artifacts/download -O process_engine.zip
 unzip process_engine.zip
 sudo apt-get install --yes ./build/*.deb
