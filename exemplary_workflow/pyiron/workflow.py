dfd0f690851033a43c06d6d38c635b6547b5aaae
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-03-12 07:44:55 -0500

    rename functions

--- exemplary_workflow/pyiron/workflow.py
+++ exemplary_workflow/pyiron/workflow.py
@@ -1,6 +1,8 @@
 import os
 from pyiron_base import Project
 
+# input parameter 
+domain_size = 2.0
 
 # create pyiron project 
 pr = Project("workflow")
@@ -16,7 +18,7 @@ for k, v in {
 
 # Preprocessing
 ## generate mesh
-def write_input(input_dict, working_directory):
+def write_input_generate_mesh(input_dict, working_directory):
     script_name = os.path.join(working_directory, "gmsh.sh")
     with open(script_name, "w") as f:
         f.writelines("gmsh -2 -setnumber domain_size " + str(input_dict["domain_size"]) + " unit_square.geo -o square.msh")
@@ -25,8 +27,8 @@ def write_input(input_dict, working_directory):
 gmsh = pr.wrap_executable(
     job_name="gmsh",
     executable_str="./gmsh.sh",
-    write_input_funct=write_input,
-    input_dict={"domain_size": 2.0},
+    write_input_funct=write_input_generate_mesh,
+    input_dict={"domain_size": domain_size},
     conda_environment_path=pr.conda_environment.preprocessing,
     input_file_lst=["../source/unit_square.geo"],
     execute_job=True,
@@ -44,14 +46,14 @@ meshio = pr.wrap_executable(
 
 # Processing
 ## poisson
-def collect_output(working_directory):
+def collect_output_poisson(working_directory):
     with open(os.path.join(working_directory, "numdofs.txt"), "r") as f:
         return {"numdofs": int(f.read())}
 
 poisson = pr.wrap_executable(
     job_name="poisson",
     executable_str="python poisson.py --mesh square.xdmf --degree 2 --outputfile poisson.pvd --num-dofs numdofs.txt",
-    collect_output_funct=collect_output,
+    collect_output_funct=collect_output_poisson,
     conda_environment_path=pr.conda_environment.processing,
     input_file_lst=["../source/poisson.py", meshio.files.square_xdmf, meshio.files.square_h5],
     execute_job=True,
@@ -69,7 +71,7 @@ pvbatch = pr.wrap_executable(
 )
 
 ## substitute macros
-def write_input(input_dict, working_directory):
+def write_input_macros(input_dict, working_directory):
     script_name = os.path.join(working_directory, "macros.sh")
     with open(script_name, "w") as f:
         f.writelines("python prepare_paper_macros.py --macro-template-file macros.tex.template --plot-data-path plotoverline.csv --domain-size " + str(input_dict["domain_size"]) + " --num-dofs " + str(input_dict["numdofs"]) + " --output-macro-file macros.tex")
@@ -78,8 +80,11 @@ def write_input(input_dict, working_directory):
 macros = pr.wrap_executable(
     job_name="macros",
     executable_str="./macros.sh",
-    write_input_funct=write_input,
-    input_dict={"domain_size": 2.0, "numdofs": 100},
+    write_input_funct=write_input_macros,
+    input_dict={
+        "domain_size": domain_size, 
+        "numdofs": poisson.output["numdofs"],
+    },
     conda_environment_path=pr.conda_environment.postprocessing,
     input_file_lst=["../source/macros.tex.template", "../source/prepare_paper_macros.py", pvbatch.files.plotoverline_csv],
     execute_job=True,
