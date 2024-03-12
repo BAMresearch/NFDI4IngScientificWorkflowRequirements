9617e02b8ee710c110a11348fb50f5a3ef5c1ef2
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-03-12 10:36:10 -0500

    Use f-strings to call executables

--- exemplary_workflow/pyiron/workflow.py
+++ exemplary_workflow/pyiron/workflow.py
@@ -18,17 +18,9 @@ for k, v in {
 
 # Preprocessing
 ## generate mesh
-def write_input_generate_mesh(input_dict, working_directory):
-    script_name = os.path.join(working_directory, "gmsh.sh")
-    with open(script_name, "w") as f:
-        f.writelines("gmsh -2 -setnumber domain_size " + str(input_dict["domain_size"]) + " unit_square.geo -o square.msh")
-    os.chmod(script_name, 0o744)
-
 gmsh = pr.wrap_executable(
     job_name="gmsh",
-    executable_str="./gmsh.sh",
-    write_input_funct=write_input_generate_mesh,
-    input_dict={"domain_size": domain_size},
+    executable_str=f"gmsh -2 -setnumber domain_size {domain_size} unit_square.geo -o square.msh",
     conda_environment_path=pr.conda_environment.preprocessing,
     input_file_lst=["../source/unit_square.geo"],
     execute_job=True,
@@ -71,20 +63,9 @@ pvbatch = pr.wrap_executable(
 )
 
 ## substitute macros
-def write_input_macros(input_dict, working_directory):
-    script_name = os.path.join(working_directory, "macros.sh")
-    with open(script_name, "w") as f:
-        f.writelines("python prepare_paper_macros.py --macro-template-file macros.tex.template --plot-data-path plotoverline.csv --domain-size " + str(input_dict["domain_size"]) + " --num-dofs " + str(input_dict["numdofs"]) + " --output-macro-file macros.tex")
-    os.chmod(script_name, 0o744)
-
 macros = pr.wrap_executable(
     job_name="macros",
-    executable_str="./macros.sh",
-    write_input_funct=write_input_macros,
-    input_dict={
-        "domain_size": domain_size, 
-        "numdofs": poisson.output["numdofs"],
-    },
+    executable_str=f"python prepare_paper_macros.py --macro-template-file macros.tex.template --plot-data-path plotoverline.csv --domain-size {domain_size} --num-dofs {poisson.output['numdofs']} --output-macro-file macros.tex",
     conda_environment_path=pr.conda_environment.postprocessing,
     input_file_lst=["../source/macros.tex.template", "../source/prepare_paper_macros.py", pvbatch.files.plotoverline_csv],
     execute_job=True,
