91060669844b863d0a63183c4a5acc3a4c7537f1
Jan Janßen <janssen@mpie.de>
2024-07-07 10:08:54 +0200

    Update pyiron_base to 0.9.6

--- exemplary_workflow/pyiron/workflow.py
+++ exemplary_workflow/pyiron/workflow.py
@@ -13,7 +13,7 @@ for k, v in {
     "processing": "../source/envs/processing.yaml",
     "postprocessing": "../source/envs/postprocessing.yaml"
 }.items():
-    pr.conda_environment.create(env_name=k, env_file=v)
+    pr.conda_environment.create(env_name=k, env_file=v, use_mamba=True, global_installation=False)
 
 
 # Preprocessing
@@ -23,7 +23,8 @@ gmsh = pr.wrap_executable(
     executable_str=f"gmsh -2 -setnumber domain_size {domain_size} unit_square.geo -o square.msh",
     conda_environment_path=pr.conda_environment.preprocessing,
     input_file_lst=["../source/unit_square.geo"],
-    execute_job=True,
+    delayed=True,
+    output_file_lst=["square.msh"],
 )
 
 ## convert mesh to xdmf
@@ -32,18 +33,26 @@ meshio = pr.wrap_executable(
     executable_str="meshio convert square.msh square.xdmf",
     conda_environment_path=pr.conda_environment.preprocessing,
     input_file_lst=[gmsh.files.square_msh],
-    execute_job=True,
+    delayed=True,
+    output_file_lst=["square.xdmf", "square.h5"],
 )
 
 
 # Processing
 ## poisson
+def collect_output(working_directory):
+    with open(os.path.join(working_directory, "numdofs.txt"), "r") as f:
+        return {"numdofs": int(f.read())}
+
 poisson = pr.wrap_executable(
     job_name="poisson",
     executable_str="python poisson.py --mesh square.xdmf --degree 2 --outputfile poisson.pvd --num-dofs numdofs.txt",
     conda_environment_path=pr.conda_environment.processing,
     input_file_lst=["../source/poisson.py", meshio.files.square_xdmf, meshio.files.square_h5],
-    execute_job=True,
+    delayed=True,
+    collect_output_funct=collect_output,
+    output_key_lst=["numdofs"],
+    output_file_lst=["poisson.pvd", "poisson000000.vtu"],
 )
 
 
@@ -54,16 +63,26 @@ pvbatch = pr.wrap_executable(
     executable_str="pvbatch postprocessing.py poisson.pvd plotoverline.csv",
     conda_environment_path=pr.conda_environment.postprocessing,
     input_file_lst=["../source/postprocessing.py", poisson.files.poisson_pvd, poisson.files.poisson000000_vtu],
-    execute_job=True,
+    delayed=True,
+    output_file_lst=["plotoverline.csv"],
 )
 
 ## substitute macros
+def write_input(input_dict, working_directory):
+    script_name = os.path.join(working_directory, "macros.sh")
+    with open(script_name, "w") as f:
+        f.writelines(f"python prepare_paper_macros.py --macro-template-file macros.tex.template --plot-data-path plotoverline.csv --domain-size {domain_size} --num-dofs {input_dict["numdofs"]} --output-macro-file macros.tex")
+    os.chmod(script_name, 0o744)
+
 macros = pr.wrap_executable(
     job_name="macros",
-    executable_str=f"python prepare_paper_macros.py --macro-template-file macros.tex.template --plot-data-path plotoverline.csv --domain-size {domain_size} --num-dofs {int(poisson.output['stdout'].split()[-1])} --output-macro-file macros.tex",
+    input_dict={"numdofs": poisson.output.numdofs},
+    write_input_funct=write_input,
+    executable_str="./macros.sh",
     conda_environment_path=pr.conda_environment.postprocessing,
     input_file_lst=["../source/macros.tex.template", "../source/prepare_paper_macros.py", pvbatch.files.plotoverline_csv],
-    execute_job=True,
+    delayed=True,
+    output_file_lst=["macros.tex"],
 )
 
 ## compile paper
@@ -72,5 +91,8 @@ tectonic = pr.wrap_executable(
     executable_str="tectonic paper.tex",
     conda_environment_path=pr.conda_environment.postprocessing,
     input_file_lst=["../source/paper.tex", macros.files.macros_tex, pvbatch.files.plotoverline_csv],
-    execute_job=True,
+    delayed=True,
 )
+
+# Execute Workflow Graph
+tectonic.pull()
